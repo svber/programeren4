49cb3efb9a8b806f44f321c27eb12cd8f37d1a43
sven berning <srjberni@avans.nl>
2017-05-03 22:39:20 +0200

    update oefening 6

--- les1/my random user.js
+++ les1/my random user.js
@@ -8,6 +8,26 @@ app.get('/', function(request, response) {
 app.get('/about', function(request, response) {
  response.send('Written by <jouw naam hier invullen>');
 })
+app.get('/json', function(request, response) {
+ response.json({
+ 		'some_name': 'Value',
+ 		'an_array_of_objects': [
+	 		{
+	 		'another_name': 'Another value',
+	 		'a_further_name': "A further value"
+	 		},
+		 	{'yet_another_name': 'Yet another value'}
+ 		],
+ 		'some_boolean': true,
+ 		'some_integer': 42,
+		'array_of_ints': [
+		 	2, 3, 5, 7, 11, 13
+	 	],
+	 	'array_of_strings': [
+		 	"twee", "drie", "vijf", "zeven"
+ 		]
+ 	})
+})
 app.post('/', function(request, response) {
  response.send('Hello Avans, POST request received!');
 })
