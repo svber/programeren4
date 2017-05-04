3fd95e39bbe04426624a0840fc772effbf44edd8
sven berning <srjberni@avans.nl>
2017-05-04 09:47:52 +0200

    tijdelijk van practicum 1

--- practicum 2/wikibooks.js
+++ practicum 2/wikibooks.js
@@ -1,18 +1,43 @@
-var http = require('http');
-
-
+var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
 
-app.all('*', function(req, res, next){
-	console.log(req.method + " " + req.url);
-	next();
-});
-
-app.get('api/v1/hello', function(req, res, next){
-	res.contentType('application/json');
-	res.json({"msg" : "hello to you all !! have a nice day."});
-});
-
-app.listen(8080, function(){
-	console.log('the magic happens at http://localhost:8080');
-});
+app.get('/', function(request, response) {
+ response.send('Hello Avans!');
+})
+app.get('/about', function(request, response) {
+ response.send('Written by <jouw naam hier invullen>');
+})
+app.get('/json', function(request, response) {
+ response.json({
+ 		'some_name': 'Value',
+ 		'an_array_of_objects': [
+	 		{
+	 		'another_name': 'Another value',
+	 		'a_further_name': "A further value"
+	 		},
+		 	{'yet_another_name': 'Yet another value'}
+ 		],
+ 		'some_boolean': true,
+ 		'some_integer': 42,
+		'array_of_ints': [
+		 	2, 3, 5, 7, 11, 13
+	 	],
+	 	'array_of_strings': [
+		 	"twee", "drie", "vijf", "zeven"
+ 		]
+ 	})
+})
+app.post('/', function(request, response) {
+ response.send('Hello Avans, POST request received!');
+})
+app.put('/', function(request, response) {
+ response.send('Hello Avans, PUT request received!');
+})
+app.all('*', function(request, response) {
+ response.status(404);
+ response.send('404 - Not found');
+})
+app.listen(port, function() {
+ console.log('Server app is listening on port' + port);
+})
