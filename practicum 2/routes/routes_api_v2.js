d3a30765b29784a1d8e7c39f2cb7ea03485b3261
sven berning <srjberni@avans.nl>
2017-05-11 12:21:47 +0200

    opdracht 4 en 5(half af)

---
+++ practicum 2/routes/routes_api_v2.js
@@ -0,0 +1,15 @@
+// API version 2
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+router.get('*', function(request, response) {
+ response.status(200);
+ response.json({
+ "description": "Recipes "
+ });
+});
+router.get('/api/v2/recipes', function(request,response){
+	response.json({recipes})
+	var recipes = require('.././recepies.js');
+});
+module.exports = router;
--- practicum 2/wikibooks.js
+++ practicum 2/wikibooks.js
@@ -44,6 +44,8 @@ app.all('*', function(request, response, next) {
 })
 
 app.use('/api/v1', require('./routes/routes_api_v1'));
+app.use('/info', require('./routes/routes_api_v2'));
+app.use('/api/v2/recipes', require('./routes/recipes'));
 
 app.all('*', function(request, response) {
  response.status(404);
